3abbb48693ed4d37872de17b605af7b73c6e7827
yasoe31 <yasoe31@gmail.com>
2015-08-21 21:46:24 +0100

    added - PlayerService

---
+++ game/src/main/java/com/yan/game/service/PlayerService.java
@@ -0,0 +1,39 @@
+package com.yan.game.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yan.game.model.Player;
+import com.yan.game.repository.PlayerRepository;
+
+@Service
+public class PlayerService {
+
+	@Autowired
+	private PlayerRepository playerRepository;
+
+	public void registerPlayer(Player player) {
+
+		playerRepository.save(player);
+	}
+
+	public void updatePlayer(Player player) {
+
+		Player p = playerRepository.findOne(player.getId());
+		p.setName(player.getName());
+		playerRepository.save(p);
+	}
+
+	public List<Player> listPlayer() {
+
+		return (List<Player>) playerRepository.findAll();
+	}
+
+	public void deletePlayer(int id) {
+
+		playerRepository.delete(id);
+	}
+
+}
